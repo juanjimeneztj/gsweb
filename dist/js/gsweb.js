fbb7afa0dd16a7397a99fdbe315b03468bbc6fcf
Juan Jimenez <juanjimeneztj@gmail.com>
2021-01-13 05:20:06 -0800

    fixed function to sms form

--- dist/js/gsweb.js
+++ dist/js/gsweb.js
@@ -1,10 +1,10 @@
 // Function to disable button in the SMS forms
 $("body").on('click','.sms-check-checkbox', function() {  
     if($('.sms-check-checkbox').prop('checked')) { 
-        $(this).parent().parent().find('button.btn').removeClass('sms-disabled'); 
-        $(this).parent().parent().find('button.btn').prop('disabled', false);                         
+        $(this).parent().parent().parent().find('button.btn').removeClass('sms-disabled'); 
+        $(this).parent().parent().parent().find('button.btn').prop('disabled', false);                         
     } else {  
-        $(this).parent().parent().find('button.btn').addClass('sms-disabled'); 
-        $(this).parent().parent().find('button.btn').prop('disabled', true); 
+        $(this).parent().parent().parent().find('button.btn').addClass('sms-disabled'); 
+        $(this).parent().parent().parent().find('button.btn').prop('disabled', true); 
     }  
-});  
+});   
