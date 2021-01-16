11cbaf2c147f359341167b8fade5ec3e628420f5
Juan Jimenez <juanjimeneztj@gmail.com>
2021-01-15 16:53:43 -0800

    added function to price label on the order forms

--- dist/js/gsweb.js
+++ dist/js/gsweb.js
@@ -7,6 +7,16 @@ $(document).ready(function(){
         srcField.addClass('sip-gsweb-order-form-'+ele);
         $('head').append('<style>.sip-nj-of-t1.sip-gsweb-order-form-'+ele+':before{ background-image: url('+srcField.attr('sip-nj-img')+');}</style>');
     });
+    $('.sip-nj-pckg-option').each(function(ele){
+        let srcField = $(this);
+        if(srcField.attr('sip-ball-message')){
+            srcField.addClass('sip-ball');
+            srcField.addClass('sip-ball-'+ele);
+            if(srcField.attr('sip-ball-color')){
+                $('head').append('<style>.sip-ball-'+ele+':before{ background: '+srcField.attr('sip-ball-color')+'!important;}</style>');
+            }
+        }
+    });
     $('.sip-nj-list-icon-check').each(function(ele){
         let srcField = $(this);
         srcField.addClass('sip-gsweb-list-'+ele);
@@ -24,6 +34,9 @@ $(document).ready(function(){
             $('head').append('<style>.sip-nj-list-icon-check.sip-gsweb-list-'+ele+' > li:before{ content: "' + srcField.attr('sip-list-icon') + '";}</style>');
         }else{
             $('head').append('<style>.sip-nj-list-icon-check.sip-gsweb-list-'+ele+' > li:before{ content: "";}</style>');
+        }
+        if(srcField.attr('sip-list-icon-color')){
+            $('head').append('<style>.sip-nj-list-icon-check.sip-gsweb-list-'+ele+' > li:before{ color: ' + srcField.attr('sip-list-icon-color') + '!important;}</style>');
         }
     });
 });
